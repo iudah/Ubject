a16ffbe243753226328f57391497f61dc2ad2fcf
iudah <olaolujude@gmail.com>
2023-09-01 19:20:29 -0700

    Remove differ() from Ubject

--- Ubject.h
+++ Ubject.h
@@ -10,12 +10,4 @@
  * @brief Represention of ADT Ubject.
  */
 extern const void *Ubject;
-
-/**
- * @brief Compares ubjects.
- * @param a an ubject
- * @param b an ubject
- * @return 1 if a and b are different, else 0
- */
-int differ(const void *a, const void *b);
 #endif
