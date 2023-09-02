7d0574513ea60827238012b8f830721a54c05808
iudah <olaolujude@gmail.com>
2023-09-02 00:40:48 -0700

    Add prototype for Point ADT and related functions

---
+++ Point.h
@@ -0,0 +1,12 @@
+/**
+ * @file Point.h
+ * @brief Prototype file of Point ADT
+ * @author Jude (iudah)
+ */
+#ifndef POINT_H
+#define POINT_H
+
+extern const void *Point;
+
+void move(void *self, int dx, int dy);
+#endif
