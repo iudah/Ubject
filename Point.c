52b4c0678b652964d18e9fdd5c089f1ecdc6f0cb
iudah <olaolujude@gmail.com>
2023-09-02 11:14:55 -0700

    Seperate Point representation to tolerate inheritance

--- Point.c
+++ Point.c
@@ -6,14 +6,11 @@
 #include <stdarg.h>
 #include <stdio.h>
 
+#include "Point.r.h"
+
 #include "new.h"
 #include "new.r.h"
 
-struct Point
-{
-    const struct Class *class;
-    int x, y;
-};
 static void *Point_ctor(void *self_, va_list *arg)
 {
     struct Point *self = self_;
