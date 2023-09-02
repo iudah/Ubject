af962387b71dbe8b11680939c79dc23dc7931894
iudah <olaolujude@gmail.com>
2023-09-02 00:37:55 -0700

    Add draw prototype for  Point

--- new.h
+++ new.h
@@ -24,24 +24,9 @@ void *new(const void *descriptor, ...);
 void delete(void *self);
 
 /**
- * @brief Create copy of an instance.
+ * @brief Display an instance.
  *
  * @param self pointer to original instance
- * @return created copy of self
  */
-void *clone(const void *self);
-
-/**
- * @brief Compares class instance.
- * @param a a class instance
- * @param b a class instance
- * @return 1 if a and b are different, else 0
- */
-int differ(const void *self, const void *b);
-
-/** @brief Get size of created instance
- * @param self pointer to item originally created by @ref new
- * @return size of self
- */
-size_t sizeOf(const void *self);
+void draw(const void *self);
 #endif
