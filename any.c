46d05cdb39ee3f6e54f59ed5aac4120fbd2d2129
iudah <olaolujude@gmail.com>
2023-09-04 01:37:35 -0700

    Implement test for circle and  point

--- any.c
+++ any.c
@@ -2,29 +2,34 @@
  * @file any.c
  * @brief Test driver
  */
-#include "Ubject.h"
-static int Any_differ(const void *self, const void *b)
-{
-    return 0;
-}
+#include "Point.h"
+#include "Circle.h"
+
+char seq_[] = " p c";
+char *seq = seq_;
 
 int main()
 {
-    void *o = new (Ubject);
-    const void *Any = new (Class, "Any", Ubject, sizeOf(o), differ, Any_differ, 0);
-    void *a = new (Any);
-    puto(Any, stdout);
-    puto(o, stdout);
-    puto(a, stdout);
+    initCircle();
 
-    if (differ(o, o) == differ(a, a))
-        puts("OK");
-    if (differ(o, a) != differ(a, o))
-        puts("NOT COMMUNICATIVE");
-
-    delete (o);
-    delete (a);
-    delete (Any);
-
+    void *pt;
+    while (*++seq)
+    {
+        switch (*seq)
+        {
+        case 'p':
+            pt = new (Point, 1, 2);
+            break;
+        case 'c':
+            pt = new (Circle, 1, 2, 3);
+            break;
+        default:
+            continue;
+        }
+        draw(pt);
+        move(pt, 10, 25);
+        draw(pt);
+        delete (pt);
+    }
     return 0;
 }
