e19f0473cd811586183c64087e40c77001c9dda6
Jude <olaolujude@gmail.com>
2023-09-05 21:03:21 +0100

    Add representation of Ubject and TypeClass
    Added the representation of Ubject and TypeClass structures and
    functions for superclass construction and destruction.

--- Ubject.r.h
+++ Ubject.r.h
@@ -5,30 +5,62 @@
 #ifndef UBJECT_R_H
 #define UBJECT_R_H
 
-/** Ubject base class*/
-struct Ubject
-{
-    /** Ubject description  */
-    const struct Class *class;
+/**
+ * @struct Ubject
+ * @brief Represents a base object with class information.
+ *
+ * This structure contains a pointer to the class descriptor of an object.
+ */
+struct Ubject {
+    /** Pointer to the class descriptor of the object. */
+    const struct BaseClass *class;
 };
 
-/** Class base class*/
-struct Class
-{
-    /** @extends Ubject */
-    const struct Ubject _;
-    const char *name;
-    const struct Class *super;
-    size_t size;
-    void *(*ctor)(void *self, va_list *arg);
+/**
+ * @struct TypeClass
+ * @brief Represents a base class with class information.
+ *
+ * This structure contains information about a class, including its name and
+ * destructor function.
+ */
+struct TypeClass {
+    /** Inherited base class structure. */
+    struct {
+        /** Pointer to the superclass descriptor. */
+        const struct BaseClass *baseclass;
+    } _;
+
+    /** Name of the class. */
+    char *name;
+
+    /** Pointer to the destructor function of the class. */
     void *(*dtor)(void *self);
-    int (*differ)(const void *self, const void *b);
-    int (*puto)(const void *self, FILE *f);
 };
 
+/** @brief Class descriptor*/
+extern const void *TypeClass;
+
+/**
+ * @brief Invoke the superclass constructor.
+ *
+ * This function invokes the superclass constructor.
+ *
+ * @param class_ Pointer to the class descriptor.
+ * @param self Pointer to the object.
+ * @param arg_ Variable argument list.
+ * @return Pointer to the initialized object.
+ */
 void *super_ctor(const void *class_, void *self, va_list *arg_);
+
+/**
+ * @brief Invoke the superclass destructor.
+ *
+ * This function invokes the superclass destructor.
+ *
+ * @param class_ Pointer to the class descriptor.
+ * @param self Pointer to the object.
+ * @return Pointer to the object after destruction.
+ */
 void *super_dtor(const void *class_, void *self);
-int super_differ(const void *class_, void *self, void *b);
-int super_puto(const void *class_, void *self, FILE *f);
 
 #endif
