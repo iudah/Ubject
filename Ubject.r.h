2936d0ed7f2b9026c454b804a46317c89db24a1c
iudah <olaolujude@gmail.com>
2023-11-19 03:27:30 +0100

    chore: Define Ubject Structure
    This commit introduces the Ubject structure in the header file
    Ubject.r.h.
    
    Key components include:
    
    - Definition of Ubject-related macros for Windows compatibility.
    - Structure definition for Ubject, featuring a pointer to the class
      descriptor and a reference count.
    
    Note: This commit is part of the ongoing refactor effort in the project.

--- Ubject.r.h
+++ Ubject.r.h
@@ -32,7 +32,7 @@
  */
 struct Ubject {
 #ifndef UBJECT_C
-  char ___[sizeof(struct _ubj_ {
+  char ___[sizeof(struct UBJECT_C_ {
 #endif
     /** Pointer to the class descriptor of the object. */
     const struct BaseClass *class_;
@@ -42,70 +42,5 @@ struct Ubject {
   })];
 #endif
 };
-
-/**
- * @struct TypeClass
- * @brief Represents a base class with class information.
- *
- * This structure contains information about a class, including its name and
- * destructor function.
- */
-struct TypeClass {
-  /** Inherited base class structure. */
-  struct {
-    /** Pointer to the class descriptor. */
-    const struct BaseClass *desc;
-    /** Pointer to the super class. */
-    const struct BaseClass *baseclass;
-    size_t _size_t;
-    void *(*voidf_)();
-  } _;
-
-  /** Name of the class. */
-  char *name;
-
-  /** Pointer to the destructor function of the class. */
-  void *(*dtor)(void *self);
-};
-
-/** @brief Class descriptor
- * @example{
- * SomeTypeClass = new (TypeClass, TypeClass,
- *                       sizeof(struct SomeTypeClass),
- *                  ctor, SomeClass_ctor, 0);
- * SomeType = new (SomeTypeClass, SomeSuperType_to_inherit_from,
- *                   sizeof(struct SomeType),
- *                   ctor, SomeType_ctor,
- *                   dtor, SomeType_dtor,
- *                   className, "SomeType",
- *                      ...,
- *                   someFnSelector, correspondingOverloadFn,
- *                   0);
- * }
- */
-extern UBJECTAPI const void *TypeClass;
-
-/**
- * @brief Invoke the superclass constructor.
- *
- * This function invokes the superclass constructor.
- *
- * @param class_ Pointer to the class descriptor.
- * @param self Pointer to the object.
- * @param arg_ Variable argument list.
- * @return Pointer to the initialized object.
- */
-UBJECTAPI void *super_ctor(const void *class_, void *self, va_list *arg_);
-
-/**
- * @brief Invoke the superclass destructor.
- *
- * This function invokes the superclass destructor.
- *
- * @param class_ Pointer to the class descriptor.
- * @param self Pointer to the object.
- * @return Pointer to the object after destruction.
- */
-UBJECTAPI void *super_dtor(const void *class_, void *self);
 
 #endif
