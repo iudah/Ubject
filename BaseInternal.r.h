03d5da84fd2f8e635966d8c752d10a7ed37d4bc8
iudah <olaolujude@gmail.com>
2023-12-31 14:10:24 +0100

    Update BaseInternal.r.h with function declarations for internal class manipulation.

--- BaseInternal.r.h
+++ BaseInternal.r.h
@@ -6,12 +6,12 @@
 
 #include "BaseClass.r.h"
 
-extern void (*base_internal_make_a_class_)(
-    void *class, const struct BaseClass *super, size_t size,
-    void *(*ctor)(void *self, va_list *arg));
-extern void (*base_internal_add_meta_class_)(void *class,
-                                             const void *meta_class);
-extern void (*base_internal_set_base_object_)(const void *class);
-extern const size_t (*class_internal_size)(const void *class);
-extern const void *(*class_internal_super)(const void *class);
+void base_internal_make_a_class_(void *class, const struct BaseClass *super,
+                                 size_t size,
+                                 void *(*ctor)(void *self, va_list *arg));
+void base_internal_add_meta_class_(void *class, const void *meta_class);
+void base_internal_set_base_object_(const void *class);
+size_t class_internal_size(const void *class);
+const void *class_internal_super(const void *class);
+int class_increment_internal_count(const void *class);
 #endif
