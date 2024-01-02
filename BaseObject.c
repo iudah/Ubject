3deea545f0fdc9afd5b41e5551a33762eabf1109
iudah <olaolujude@gmail.com>
2024-01-02 21:04:11 +0100

    Refactor BaseObject.c for Improved Readability and Maintainability
    Refactor BaseObject.c to improve code clarity and maintainability:
    - Introduce comments for better documentation.
    - Organize include statements and define macros for better readability.
    - Enhance error and warning handling functions with comments.
    - Implement and document the BaseObject class constructor.
    - Add comments for functions related to class, meta class, and descriptors.
    - Improve naming conventions for internal functions and variables.
    - Ensure consistent use of static and attribute annotations.

--- BaseObject.c
+++ BaseObject.c
@@ -1,17 +1,24 @@
-
+// Include standard C library headers for input/output and general purpose
+// functions
 #include <stdio.h>
 #define __STDC_WANT_LIB_EXT1__ 1 // required for memcpy_s
 #include <stdlib.h>
 
+// Include header files for the base class and base object
 #include "BaseClass.h"
 
-// #define BASEOBJECT_EXPORTS
+// Include the header file for the BaseObject module
 #include "BaseObject.h"
 
+// Define a macro to indicate that we are implementing the BaseObject module
 #define BASEOBJECT_C
+// Include internal header files for the BaseObject module
 #include "BaseInternal.r.h"
 #include "BaseObject.r.h"
 
+// Error and Warning Handling Functions:
+// These functions handle errors and warnings by printing messages to the
+// console and aborting the program.
 static void Ub_error(const char *fmt, ...) {
   va_list arg;
   puts("\e[0;31m");
@@ -19,7 +26,7 @@ static void Ub_error(const char *fmt, ...) {
   vfprintf(stderr, fmt, arg);
   va_end(arg);
   puts("\e[0m");
-  exit(-1);
+  abort();
 }
 static void Ub_warn(const char *fmt, ...) {
   va_list arg;
@@ -31,10 +38,21 @@ static void Ub_warn(const char *fmt, ...) {
   return;
 }
 
-const struct Ubject_err_disp__ UbjectError = {Ub_error, Ub_warn};
+// Define the UbjectError structure for error and warning handling
+const struct Ubject_err_disp__ UbjectError = {.error = Ub_error,
+                                              .warn = Ub_warn};
 
-static void *BaseObject_ctor(void *self_, va_list *arg) { return self_; }
+// Base Object Class Constructor:
+// This function is the constructor for the base object class. It takes a
+// pointer to the object being constructed and a variable argument list, and it
+// initializes the object.
+static void *BaseObject_ctor(void *self_,
+                             va_list __attribute__((unused)) * arg) {
+  return self_;
+}
 
+// Function to Get the Class of an Object:
+// This function returns the class of an object.
 const void *classOf(const void *self_) {
   const struct BaseObject *self = self_;
   if (!self) {
@@ -46,24 +64,33 @@ const void *classOf(const void *self_) {
   return self->class;
 }
 
+// Function to Initialize the Base Object Class:
+// This function initializes the base object class. It is called as a
+// constructor with a high priority.
 static void __attribute__((__constructor__(BASE_CTOR_PRIORITY)))
 initBaseObject(void) {}
 
-static void base_internal_add_meta_class(void *class, const void *meta_class) {
+// Function to Add a Meta Class to a Class:
+// This function adds a meta class to a class.
+void base_internal_add_meta_class_(void *class, const void *meta_class) {
   struct BaseObject *bo = class;
   bo->class = (void *)meta_class;
 }
 
-void (*base_internal_add_meta_class_)(void *class, const void *meta_class) =
-    base_internal_add_meta_class;
-
+// Pointer to the Base Object Class Descriptor:
+// This pointer is used to access the base object class descriptor.
 const void *BaseObject = 0;
 
-static void setBaseObjectDesc(const void *base_obj_desc) {
+// Function to Set the Base Object Descriptor:
+// This function sets the base object descriptor.
+void base_internal_set_base_object_(const void *base_obj_desc) {
   BaseObject = base_obj_desc;
 }
-void (*base_internal_set_base_object_)(const void *class) = setBaseObjectDesc;
 
+// Function to Initialize an Object:
+// This function initializes an object of a given class. It takes a pointer to
+// the class of the object being initialized and a variable argument list, and
+// it returns a pointer to the initialized object.
 void *init(const void *class_, ...) {
   const struct BaseClass *class = class_;
   size_t class_size = class_internal_size(class);
@@ -82,5 +109,13 @@ void *init(const void *class_, ...) {
   object = ctor(object, &arg);
   va_end(arg);
 
+  object->instance_index = class_increment_internal_count(class);
+
   return object;
+}
+
+// Function to Get the instance index of an Object:
+// This function returns the instance index of an object.
+int ubjectIndex(const void *self_) {
+  return ((struct BaseObject *)(self_))->instance_index;
 }
